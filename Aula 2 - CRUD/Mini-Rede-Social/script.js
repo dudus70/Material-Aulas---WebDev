df2d030cfeecdba8b0284178d46ed29cbf64ef21
Eduardo <dududelciel@gmail.com>
2025-08-14 21:42:21 -0300

    webDev-aula

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,58 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+    mostrarPost();
+    document.querySelector("#postForm").addEventListener("submit",addPost)
+}
+
+//CREATE 
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault();
+
+    const textoPost = document.querySelector("#postText").value;
+    const cateriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+    const dataPost = new Date().toLocaleString();
+
+    const novoPost = {
+        text: textoPost,
+        category: cateriaPost,
+        image: imagemPost,
+        date: dataPost,
+    }
+
+    posts.unshift(novoPost);
+
+    mostrarPost()
+}
+
+//READ
+function mostrarPost(){
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ""
+
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+        <h2>${pegaItem.text}</h2>
+        <img src="${pegaItem.image}"/>
+        <p>Categoria:${pegaItem.category}</p>
+        <p>Data e Hora:${pegaItem.date}</p>
+        <button>Editar</button>
+        <button>Apagar</button>
+        `
+        listaPosts.append(cardPost)
+
+    })
+}
+
+
+//UPDATE
+function editarPost(){}
+
+//DELETE
+function deletarPost(){}
